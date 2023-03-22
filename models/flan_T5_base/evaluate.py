5d0d24d45cce7a0de94e0e611972657987ae7118
LeoZotos <zotos.leo@gmail.com>
2023-03-22 14:50:04 +0100

    new branch, new me. start of bart score implementation

--- models/flan_T5_base/evaluate.py
+++ models/flan_T5_base/evaluate.py
@@ -1,3 +1,8 @@
+from bart_score import BARTScorer
 
 def run(args):
-    print('hello world')
+    bart_scorer = BARTScorer(device='cuda:0', checkpoint='facebook/bart-large-cnn')
+    bart_scorer.load(path='bart.pth')
+    bart_scorer.score(['This is interesting.'], ['This is fun.'], batch_size=4)
+
+run(None)
