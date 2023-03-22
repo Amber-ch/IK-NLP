35a52eaa96f2475c9fedcce1be7e657a7019f194
LeoZotos <zotos.leo@gmail.com>
2023-03-22 16:03:55 +0100

    made evaluate template

--- models/flan_T5_base/evaluate.py
+++ models/flan_T5_base/evaluate.py
@@ -1,8 +1,21 @@
+import torch
 from bart_score import BARTScorer
 
 def run(args):
-    bart_scorer = BARTScorer(device='cuda:0', checkpoint='facebook/bart-large-cnn')
-    bart_scorer.load(path='bart.pth')
-    bart_scorer.score(['This is interesting.'], ['This is fun.'], batch_size=4)
+    bart_scorer = BARTScorer(device="cuda" if torch.cuda.is_available() else "cpu", checkpoint='facebook/bart-large-cnn')
+    bart_scorer.load(path='models/bart.pth')
+    
+    # We load data here. For now placeholders.
+    input = ["I'm super happy today.", "This is a good idea."]
+    target = [["I feel good today.", "I feel sad today."], ["Not bad.", "Sounds like a good idea."]]
+    
+    # We generate the predictions, for now placeholders.
+    generatedResponses = ["This is the output for one.", "This is the output for two"]
+    
+    
+    results = bart_scorer.multi_ref_score(generatedResponses, target, agg="max", batch_size=4)
+    
+    print(results)
+    print("average score: ", sum(results)/len(results))
 
 run(None)
